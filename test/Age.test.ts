87eff2595815234451ed0ebc7f078db913353a88
DSoloha <dsoloha@live.com>
2021-09-06 21:04:48 -0400

    Added age tests

--- test/Age.test.ts
+++ test/Age.test.ts
@@ -4,6 +4,14 @@ test('default birthday', () => {
   expect(new Age().birth.day).toBe(1)
 })
 
-test('given birthday', () => {
+test('given birth day', () => {
   expect(new Age({birth: {day: 12}}).birth.day).toBe(12)
+})
+
+test('given birth month', () => {
+  expect(new Age({birth: {month: 5}}).birth.month).toBe(5);
+})
+
+test('given birth year', () => {
+  expect(new Age({birth: {year: 99}}).birth.year).toBe(99)
 })
