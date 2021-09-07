72de5abfd85e28d76ce643c4cb1acd1f881e88d3
DSoloha <dsoloha@live.com>
2021-09-07 09:16:43 -0400

    Updated tests

--- test/Hair.test.ts
+++ test/Hair.test.ts
@@ -1,15 +1,15 @@
 import { Hair } from '../src/index'
 
 test('default hair color', () => {
-  expect(new Hair().color).toBe('black')
+  expect(new Hair().color).toBe('')
 })
 
 test('default hair length', () => {
-  expect(new Hair().length).toBe('short')
+  expect(new Hair().length).toBe('')
 })
 
 test('default hair style', () => {
-  expect(new Hair().style).toBe('straight')
+  expect(new Hair().style).toBe('')
 })
 
 test('given hair color', () => {
