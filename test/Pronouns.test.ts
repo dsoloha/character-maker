f3176fca2a50d78a6259ca8ddd10d3db3f7b8d39
DSoloha <dsoloha@live.com>
2021-09-06 21:46:29 -0400

    Added more pronoun tests

--- test/Pronouns.test.ts
+++ test/Pronouns.test.ts
@@ -1,19 +1,26 @@
 import { Pronouns } from '../src/index'
 
-test('default male pronouns', () => {
-  expect(new Pronouns('male').He).toBe('He')
-})
-
 test('default subject', () => {
   expect(new Pronouns('male').subject).toBe('he')
+  expect(new Pronouns('female').he).toBe('she')
+  expect(new Pronouns('plural').He).toBe('They')
+  expect(new Pronouns('neuter').she).toBe('it')
+  expect(new Pronouns('male').She).toBe('He')
 })
 
 test('default object', () => {
+  expect(new Pronouns('male').Him).toBe('Him')
   expect(new Pronouns('female').object).toBe('her')
+  expect(new Pronouns('plural').him).toBe('them')
+  expect(new Pronouns('neuter').Her).toBe('It')
 })
 
 test('default possessive', () => {
+  expect(new Pronouns('male').His).toBe('His')
+  expect(new Pronouns('female').hers).toBe('her')
   expect(new Pronouns('plural').possessive).toBe('their')
+  expect(new Pronouns('neuter').his).toBe('its')
+  expect(new Pronouns('male').Hers).toBe('His')
 })
 
 test('default possessivePronoun', () => {
@@ -25,5 +32,9 @@ test('default noun', () => {
 })
 
 test('default reflexive', () => {
+  expect(new Pronouns('male').himself).toBe('himself')
   expect(new Pronouns('female').reflexive).toBe('herself')
+  expect(new Pronouns('plural').himself).toBe('themselves')
+  expect(new Pronouns('neuter').Herself).toBe('Itself')
+  expect(new Pronouns('male').Himself).toBe('Himself')
 })
