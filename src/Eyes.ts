5b29cc27cb8493ed27fab03cac79e3e2675f960d
DSoloha <dsoloha@live.com>
2021-09-21 17:00:30 -0400

    Split multi-colored eyes from .color

--- src/Eyes.ts
+++ src/Eyes.ts
@@ -1,22 +1,26 @@
 export default class Eyes {
-  /** The player's eye color or eye colors if different. */
-  color:
-    | string
-    | {
-        /** The character's left eye color. */
-        left: string
-        /** The character's right eye color. */
-        right: string
-      }
+  /** The character's eye color. */
+  color: string
+
+  /** The character's eye colors if different. */
+  colors: {
+    /** The character's left eye color. */
+    left: string
+    /** The character's right eye color. */
+    right: string
+  }
 
   constructor(options?: {
-    color?:
-      | string
-      | {
-          left: string
-          right: string
-        }
+    color?: string
+    colors?: {
+      left: string
+      right: string
+    }
   }) {
     this.color = options?.color ?? ''
+    this.colors = options?.colors ?? {
+      left: this.color,
+      right: this.color,
+    }
   }
 }
