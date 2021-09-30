759a02545bb95a279ab8e802bedf3bf76e1d1cd3
DSoloha <dsoloha@live.com>
2021-09-30 00:44:38 -0400

    Added education

--- src/Background.ts
+++ src/Background.ts
@@ -3,9 +3,27 @@ export default class Background {
   birthplace: string
   /** The character's nationality. */
   nationality: string
+  /** The character's education. */
+  education: {
+    /** The school the character went to. */
+    school?: string
+    /** How many years the character attended the school. */
+    length?: number
+  }
 
-  constructor(options?: { birthplace?: string; nationality?: string }) {
+  constructor(options?: {
+    birthplace?: string
+    nationality?: string
+    education?: {
+      school?: string
+      length?: number
+    }
+  }) {
     this.birthplace = options?.birthplace ?? ''
     this.nationality = options?.nationality ?? ''
+    this.education = options?.education ?? {
+      school: options?.education?.school ?? '',
+      length: options?.education?.length ?? 1,
+    }
   }
 }
--- test/Background.test.ts
+++ test/Background.test.ts
@@ -11,3 +11,11 @@ test('given birthplace', () => {
 test('given nationality', () => {
   expect(new Background({nationality: 'English'}).nationality).toBe('English')
 })
+
+test('given school', () => {
+  expect(new Background({education: {school: 'Oxford'}}).education.school).toBe('Oxford')
+})
+
+test('given education length', () => {
+  expect(new Background({education: {length: 4}}).education.length).toBe(4)
+})
