947f4c5536e2255612a806eda60f4da989a50afd
DSoloha <dsoloha@live.com>
2021-09-30 19:44:16 -0400

    Added astigmatism, farsightedness, and nearsightedness

--- src/Eyes.ts
+++ src/Eyes.ts
@@ -1,7 +1,8 @@
 export default class Eyes {
+  /** Whether the character has astigmatism. */
+  astigmatism: boolean
   /** The character's eye color. */
   color: string
-
   /** The character's eye colors if different. */
   colors: {
     /** The character's left eye color. */
@@ -9,18 +10,28 @@ export default class Eyes {
     /** The character's right eye color. */
     right: string
   }
+  /** Whether the character is farsighted. */
+  farsighted: boolean
+  /** Whether the character is nearsighted. */
+  nearsighted: boolean
 
   constructor(options?: {
+    astigmatism?: boolean
     color?: string
     colors?: {
       left: string
       right: string
     }
+    farsighted?: boolean
+    nearsighted?: boolean
   }) {
+    this.astigmatism = options?.astigmatism ?? false
     this.color = options?.color ?? ''
     this.colors = options?.colors ?? {
       left: this.color,
       right: this.color,
     }
+    this.farsighted = options?.farsighted ?? false
+    this.nearsighted = options?.nearsighted ?? false
   }
 }
--- test/Eyes.test.ts
+++ test/Eyes.test.ts
@@ -4,6 +4,18 @@ test('default eyes', () => {
   expect(new Eyes().color).toBe('')
 });
 
+test('given astigmatism', () => {
+  expect(new Eyes({ astigmatism: true }).astigmatism).toBe(true)
+})
+
 test('given eye color', () => {
   expect(new Eyes({ color: 'red' }).color).toBe('red')
 });
+
+test('given farsightedness', () => {
+  expect(new Eyes({ farsighted: true }).farsighted).toBe(true)
+})
+
+test('given nearsightedness', () => {
+  expect(new Eyes({ nearsighted: true}).nearsighted).toBe(true)
+})
