4c8b7f23c45e1aa7de67094afe73d86035ad1a35
DSoloha <dsoloha@live.com>
2021-10-02 10:55:36 -0400

    Removed duplicate file

--- test/Speech.ts
+++
@@ -1,9 +0,0 @@
-import { Speech } from '../src/index'
-
-test('default speech', () => {
-  expect(new Speech().style).toBe('')
-})
-
-test('given speech style', () => {
-  expect(new Speech({ style: 'posh' }).style).toBe('posh')
-})
