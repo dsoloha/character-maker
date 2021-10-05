faf304fde7742da0967ed50c8951726ea291b421
DSoloha <dsoloha@live.com>
2021-10-04 23:40:00 -0400

    Split default character tests into smaller tests

--- test/Character.test.ts
+++ test/Character.test.ts
@@ -1,43 +1,59 @@
 import { Character } from '../src/index'
 
 // default
-test('default character', () => {
+test('default character age', () => {
   expect(new Character().age.birth.day).toBe(1)
   expect(new Character().age.birth.month).toBe(1)
   expect(new Character().age.birth.year).toBe(1)
+})
 
+test('default character background', () => {
   expect(new Character().background.birthplace).toBe('')
   expect(new Character().background.education).toBeNull()
   expect(new Character().background.nationality).toBe('')
   expect(new Character().background.occupation).toEqual({ type: '', income: 0, length: 0 })
+})
 
+test('default character eyes', () => {
   expect(new Character().eyes.astigmatism).toBe(false)
   expect(new Character().eyes.color).toBe('')
   expect(new Character().eyes.colors).toEqual({ right: '', left: '' })
   expect(new Character().eyes.farsighted).toBe(false)
   expect(new Character().eyes.nearsighted).toBe(false)
+})
 
+test('default character hair', () => {
   expect(new Character().hair.arm.color).toBe('')
   expect(new Character().hair.arm.length).toBe('')
   expect(new Character().hair.arm.style).toBe('')
+})
 
+test('default character name', () => {
   expect(new Character().name.first).toBe('')
   expect(new Character().name.middle).toBeNull()
   expect(new Character().name.last).toBeNull()
   expect(new Character().name.full).toBe('')
   expect(new Character().name.nicknames).toHaveLength(0)
+})
 
+test('default character nose', () => {
   expect(new Character().nose.anosmia).toBe(false)
+})
 
+test('default character pronouns', () => {
   expect(new Character().pronouns.subject).toBe('he')
   expect(new Character().pronouns.object).toBe('him')
   expect(new Character().pronouns.possessive).toBe('his')
   expect(new Character().pronouns.possessivePronoun).toBe('his')
   expect(new Character().pronouns.noun).toBe('man')
   expect(new Character().pronouns.reflexive).toBe('himself')
+})
 
+test('default character sex', () => {
   expect(new Character().sex.type).toBe('male')
+})
 
+test('default character skin', () => {
   expect(new Character().skin.birthmarks).toBeNull()
   expect(new Character().skin.brands).toBeNull()
   expect(new Character().skin.color).toBe('')
