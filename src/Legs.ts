42198c57cfee58b66ec356071a6fe247e781e870
DSoloha <dsoloha@live.com>
2021-10-08 17:04:08 -0400

    Added hands and feet

--- src/Legs.ts
+++ src/Legs.ts
@@ -1,18 +1,24 @@
+import Foot, { IFoot } from './Foot'
+
 export default class Legs implements ILegs {
   left: ILeg
   right: ILeg
 
   constructor(options?: ILegs) {
     this.left = options?.left ?? {
+      foot: options?.left?.foot ?? new Foot(options?.left?.foot),
       size: options?.left?.size ?? '',
     }
     this.right = options?.right ?? {
+      foot: options?.right?.foot ?? new Foot(options?.right?.foot),
       size: options?.right?.size ?? '',
     }
   }
 }
 
 export interface ILeg {
+  /** Properties pertaining to the foot on the leg. */
+  foot: IFoot
   /** Properties pertaining to the leg's size. */
   size: string
 }
