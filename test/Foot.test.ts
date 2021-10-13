83b00ff55a9b78886f730a6ecfd8cbe92851c122
DSoloha <dsoloha@live.com>
2021-10-12 22:30:42 -0400

    Added missing tests

--- test/Foot.test.ts
+++ test/Foot.test.ts
@@ -1,6 +1,9 @@
 import { Foot } from '../src/index'
 
-test('default hands', () => {
+test('default feet', () => {
   expect(new Foot().toes).toBe(5)
-  expect(new Foot().toes).toBe(5)
+})
+
+test('given feet', () => {
+  expect(new Foot({ toes: 3 }).toes).toBe(3)
 })
--- test/Hand.test.ts
+++ test/Hand.test.ts
@@ -2,5 +2,8 @@ import { Hand } from '../src/index'
 
 test('default hands', () => {
   expect(new Hand().fingers).toBe(5)
-  expect(new Hand().fingers).toBe(5)
+})
+
+test('given hands', () => {
+  expect(new Hand({ fingers: 3 }).fingers).toBe(3)
 })
