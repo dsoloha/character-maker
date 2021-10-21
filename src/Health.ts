75cf9d6211562cbe81217e0e5156f9374a2731a9
DSoloha <dsoloha@live.com>
2021-10-21 18:43:25 -0400

    Added generate() to Health

--- src/Health.ts
+++ src/Health.ts
@@ -1,3 +1,5 @@
+import { gaussian } from '../lib/number'
+
 export default class Health implements IHealth {
   illnesses: string[]
   injuries: string[]
@@ -5,6 +7,29 @@ export default class Health implements IHealth {
   constructor(options?: IHealth) {
     this.illnesses = options?.illnesses ?? []
     this.injuries = options?.injuries ?? []
+  }
+
+  generateIllnesses(): string[] {
+    const illnesses: string[] = []
+
+    if (gaussian(1, 100) > 80) illnesses.push('common cold')
+
+    return illnesses
+  }
+
+  generateInjuries(): string[] {
+    const injuries: string[] = []
+
+    if (gaussian(1, 100) > 80) injuries.push('cut')
+
+    return injuries
+  }
+
+  generate(): IHealth {
+    return {
+      illnesses: this.generateIllnesses(),
+      injuries: this.generateInjuries(),
+    }
   }
 }
 
