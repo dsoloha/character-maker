792bb8b02c71a7c3f69d7e37386636cd808fbc6a
DSoloha <dsoloha@live.com>
2021-10-21 14:39:32 -0400

    Added generate to Build

--- src/Build.ts
+++ src/Build.ts
@@ -1,3 +1,7 @@
+import { random } from '../lib/array';
+import { gaussian } from '../lib/number'
+import { SexType } from './Sex'
+
 export default class Build implements IBuild {
   height: number
   muscles: string
@@ -9,6 +13,41 @@ export default class Build implements IBuild {
     this.muscles = options?.muscles ?? ''
     this.type = options?.type ?? ''
     this.weight = options?.weight ?? 0
+  }
+
+  // TODO: generate height based on nationality
+  generateHeight(options: { sex: SexType } = { sex: 'male' }): number {
+    if (options?.sex === 'male') return gaussian(147, 207)
+    else if (options?.sex === 'female') return gaussian(134, 194)
+
+    return gaussian(140, 200)
+  }
+
+  // TODO: base muscles on sex and build type
+  generateMuscles(options: { sex: SexType } = { sex: 'male' }): string {
+    const muscles = ['weak', 'average', 'strong', 'jacked']
+
+    if (options?.sex === 'male') return random(muscles)
+    else if (options?.sex === 'female') return random(muscles)
+
+    return random(muscles)
+  }
+
+  // TODO: add more types
+  generateType(options: { sex: SexType } = { sex: 'male' }): string {
+    const types = ['lithe', 'average', 'stocky']
+
+    if (options?.sex === 'male') return random(types)
+    else if (options?.sex === 'female') return random(types)
+
+    return random(types)
+  }
+
+  generateWeight(options: { sex: SexType } = { sex: 'male' }): number {
+    if (options?.sex === 'male') return gaussian()
+    else if (options?.sex === 'female') return gaussian()
+
+    return gaussian()
   }
 }
 
