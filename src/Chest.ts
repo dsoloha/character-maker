e836883f4fd3491041c6e151b6973c2a81763ca0
DSoloha <dsoloha@live.com>
2021-10-21 16:36:39 -0400

    Added generate() to Chest

--- src/Chest.ts
+++ src/Chest.ts
@@ -1,8 +1,22 @@
+import { random } from '../lib/array'
+
 export default class Chest implements IChest {
   size: string
 
   constructor(options?: IChest) {
     this.size = options?.size ?? ''
+  }
+
+  generateChest(): string {
+    const sizes = ['flat', 'small', 'medium', 'large', 'huge']
+
+    return random(sizes)
+  }
+
+  generate(): IChest {
+    return {
+      size: this.generateChest(),
+    }
   }
 }
 
