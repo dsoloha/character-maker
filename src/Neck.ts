2f75263ed7c8e3bb4fac8ac2788d742db41fa6f9
DSoloha <dsoloha@live.com>
2021-10-21 22:36:20 -0400

    Added generate() to Neck

--- src/Neck.ts
+++ src/Neck.ts
@@ -1,8 +1,20 @@
+import { gaussian } from '../lib/number'
+
 export default class Neck implements INeck {
   larynx: boolean
 
   constructor(options?: INeck) {
     this.larynx = options?.larynx ?? true
+  }
+
+  generateLarynx(): boolean {
+    return gaussian(1, 100) > 99 ? false : true
+  }
+
+  generate(): INeck {
+    return {
+      larynx: this.generateLarynx(),
+    }
   }
 }
 
