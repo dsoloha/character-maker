1f9ae4d559f447d13e043fbce5af16e3aba55f49
DSoloha <dsoloha@live.com>
2021-10-21 23:13:51 -0400

    Added generate() to Stomach

--- src/Stomach.ts
+++ src/Stomach.ts
@@ -1,8 +1,23 @@
+import '../lib/array'
+
 export default class Stomach implements IStomach {
   shape: string
 
   constructor(options?: IStomach) {
     this.shape = options?.shape ?? ''
+  }
+
+  generateShape(): string {
+    const shapes = ['flat', 'round']
+    const shape = shapes.random()
+
+    return shape
+  }
+
+  generate(): IStomach {
+    return {
+      shape: this.generateShape(),
+    }
   }
 }
 
