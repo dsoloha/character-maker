554ccf4da0e104dd2487572790209ae532fc8851
DSoloha <dsoloha@live.com>
2021-10-21 20:57:53 -0400

    Added generate() to Legs

--- src/Legs.ts
+++ src/Legs.ts
@@ -1,3 +1,4 @@
+import { gaussian } from '../lib/number'
 import Foot, { IFoot } from './Foot'
 
 export default class Legs implements ILegs {
@@ -6,19 +7,46 @@ export default class Legs implements ILegs {
 
   constructor(options?: ILegs) {
     this.left = options?.left ?? {
-      foot: options?.left?.foot ?? new Foot(options?.left?.foot),
+      foot: options?.left?.foot ?? null,
       size: options?.left?.size ?? '',
     }
     this.right = options?.right ?? {
-      foot: options?.right?.foot ?? new Foot(options?.right?.foot),
+      foot: options?.right?.foot ?? null,
       size: options?.right?.size ?? '',
     }
+  }
+
+  generateFoot(): IFoot | null {
+    return gaussian(1, 100) > 95 ? null : new Foot().generate()
+  }
+
+  generateLeg(): ILeg {
+    return {
+      foot: this.generateFoot(),
+      size: '',
+    }
+  }
+
+  generate(): ILegs {
+    const left = this.generateLeg()
+    const right = this.generateLeg()
+    const sizes = ['small', 'average', 'large', 'huge']
+    const size: string = sizes.random()
+
+    left.size = size
+    right.size = size
+
+    return {
+      left,
+      right,
+    }
+  }
   }
 }
 
 export interface ILeg {
   /** Properties pertaining to the foot on the leg. */
-  foot?: IFoot
+  foot?: IFoot | null
   /** Properties pertaining to the leg's size. */
   size?: string
 }
