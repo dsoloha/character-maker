2feaffd2edf3e71aedd656573e57816f7eaf64d7
DSoloha <dsoloha@live.com>
2021-10-21 22:00:35 -0400

    Added generate() to Mouth

--- src/Mouth.ts
+++ src/Mouth.ts
@@ -1,3 +1,6 @@
+import '../lib/array'
+import { gaussian, random } from '../lib/number'
+
 export default class Mouth implements IMouth {
   teeth: {
     count: number
@@ -12,16 +15,40 @@ export default class Mouth implements IMouth {
     }
     this.tongue = options?.tongue ?? true
   }
+
+  generateTeeth(): ITeeth {
+    const count = gaussian(1, 100) > 95 ? random(1, 32) : 32
+    const descriptions = ['straight', 'yellow', 'white']
+    const description = descriptions.random()
+
+    return {
+      count,
+      description,
+    }
+  }
+
+  generateTongue(): boolean {
+    return gaussian(1, 100) > 99 ? false : true
+  }
+
+  generate(): IMouth {
+    return {
+      teeth: this.generateTeeth(),
+      tongue: this.generateTongue(),
+    }
+  }
+}
+
+export interface ITeeth {
+  /** How many teeth the character has, out of 32. */
+  count: number
+  /** A description of the character's teeth. */
+  description: string
 }
 
 export interface IMouth {
   /** Properties pertaining to the character's teeth. */
-  teeth?: {
-    /** How many teeth the character has, out of 32. */
-    count: number
-    /** A description of the character's teeth. */
-    description: string
-  }
+  teeth?: ITeeth
   /** Whether or not the character has a tongue. */
   tongue?: boolean
 }
