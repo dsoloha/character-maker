f9939efb7eda07687ce2b7543f2905eca9d4d0ac
Dan <dcoded@live.com>
2022-01-26 23:33:37 -0500

    Added generate functions to Background

--- src/Background.ts
+++ src/Background.ts
@@ -1,3 +1,5 @@
+import { random } from '../lib/number'
+
 export default class Background implements IBackground {
   birthplace: string
   education: {
@@ -21,27 +23,77 @@ export default class Background implements IBackground {
       length: options?.occupation?.length ?? 0,
     }
   }
+
+  generateBirthplace(): string {
+    // TODO: hook this up to a list of potential birthplaces
+    const places: string[] = []
+
+    return places.random()
+  }
+
+  generateEducation(): IEducation | null {
+    const seed = random(1, 100)
+
+    if (seed < 25) return null
+
+    return {
+      school: '',
+      length: 0,
+    }
+  }
+
+  generateNationality(): string {
+    const nationalities: string[] = []
+
+    return nationalities.random()
+  }
+
+  generateOccupation(): IOccupation {
+    // TODO: hook this up to a list of potential occupations
+    const types: string[] = []
+    const income = random(1, 100)
+    const length = random(1, 100)
+
+    return {
+      type: types.random(),
+      income,
+      length,
+    }
+  }
+
+  generate(): IBackground {
+    return {
+      birthplace: this.generateBirthplace(),
+      education: this.generateEducation(),
+      nationality: this.generateNationality(),
+      occupation: this.generateOccupation(),
+    }
+  }
 }
 
 export interface IBackground {
   /** The character's birthplace. */
   birthplace?: string
   /** Properties pertaining to the character's education. */
-  education?: {
-    /** The school the character went to. */
-    school: string
-    /** How many years the character attended the school. */
-    length: number
-  } | null
+  education?: IEducation | null
   /** The character's nationality. */
   nationality?: string
   /** Properties pertaining to the character's occupation. */
-  occupation?: {
-    /** The character's current occupation. */
-    type: string
-    /** The character's annual income in USD. */
-    income: number
-    /** How many years the character has had this occupation. */
-    length: number
-  }
+  occupation?: IOccupation
+}
+
+export interface IEducation {
+  /** The school the character went to. */
+  school: string
+  /** How many years the character attended the school. */
+  length: number
+}
+
+export interface IOccupation {
+  /** The character's current occupation. */
+  type: string
+  /** The character's annual income in USD. */
+  income: number
+  /** How many years the character has had this occupation. */
+  length: number
 }
