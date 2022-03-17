aa0ce86b0279fb5d61cfab1f7fad4160c075e7cb
Dan <dcoded@live.com>
2022-03-17 13:55:34 -0400

    Added some documentation

--- src/Build.ts
+++ src/Build.ts
@@ -16,46 +16,50 @@ export default class Build implements IBuild {
   }
 
   // TODO: generate height based on nationality
-  generateHeight(options: { sex: SexType } = { sex: 'male' }): number {
+  /** Generates a new height, in cm. */
+  private generateHeight(options: { sex: SexType } = { sex: 'male' }): number {
     if (options?.sex === 'male') return gaussian(147, 207)
-    else if (options?.sex === 'female') return gaussian(134, 194)
+    if (options?.sex === 'female') return gaussian(134, 194)
 
     return gaussian(140, 200)
   }
 
   // TODO: base muscles on sex and build type
-  generateMuscles(options: { sex: SexType } = { sex: 'male' }): string {
+  /** Generates new muscles. */
+  private generateMuscles(options: { sex: SexType } = { sex: 'male' }): string {
     const muscles = ['weak', 'average', 'strong', 'jacked']
 
     if (options?.sex === 'male') return muscles.random()
-    else if (options?.sex === 'female') return muscles.random()
+    if (options?.sex === 'female') return muscles.random()
 
     return muscles.random()
   }
 
   // TODO: add more types
-  generateType(options: { sex: SexType } = { sex: 'male' }): string {
+  /** Generates a new build. */
+  private generateType(options: { sex: SexType } = { sex: 'male' }): string {
     const types = ['lithe', 'average', 'stocky']
 
     if (options?.sex === 'male') return types.random()
-    else if (options?.sex === 'female') return types.random()
+    if (options?.sex === 'female') return types.random()
 
     return types.random()
   }
 
-  generateWeight(options: { sex: SexType } = { sex: 'male' }): number {
+  /** Generates a new weight, in kg. */
+  private generateWeight(options: { sex: SexType } = { sex: 'male' }): number {
     if (options?.sex === 'male') return gaussian()
-    else if (options?.sex === 'female') return gaussian()
+    if (options?.sex === 'female') return gaussian()
 
     return gaussian()
   }
 
-  generate(): IBuild {
+  generate(options: { sex: SexType } = { sex: 'male' }): IBuild {
     return {
-      height: this.generateHeight(),
-      muscles: this.generateMuscles(),
-      type: this.generateType(),
-      weight: this.generateWeight(),
+      height: this.generateHeight({ sex: options?.sex }),
+      muscles: this.generateMuscles({ sex: options?.sex }),
+      type: this.generateType({ sex: options?.sex }),
+      weight: this.generateWeight({ sex: options?.sex }),
     }
   }
 }
