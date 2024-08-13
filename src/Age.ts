f70a97bd0446863619dd568e6a52198dce7ab84c
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-12 19:41:29 -0500

    Improved age generation, added monthsOld() getter

--- src/Age.ts
+++ src/Age.ts
@@ -8,10 +8,14 @@ export default class Age implements IAge {
   }
 
   constructor(options?: IAge) {
+    const year = options?.birth?.year ?? this.generateBirthYear()
+    const month = options?.birth?.month ?? this.generateBirthMonth()
+    const day = options?.birth?.day ?? this.generateBirthDay(year, month)
+
     this.birth = {
-      day: options?.birth?.day ?? 1,
-      month: options?.birth?.month ?? 1,
-      year: options?.birth?.year ?? 1,
+      day,
+      month,
+      year,
     }
   }
 
@@ -20,36 +24,58 @@ export default class Age implements IAge {
     if (year % 4 === 0 && month === 2) return random(1, 29)
     if (month === 2) return random(1, 28)
     if ([1, 3, 5, 6, 8, 10, 12].includes(month)) return random(1, 31)
+
     return random(1, 30)
   }
 
   /** Generates a new birth month. */
-  private generateBirthMonth(): number {
-    return random(1, 12)
+  private generateBirthMonth({ month } = { month: 0 }): number {
+    month = month || random(1, 12)
+
+    return month
   }
 
   /** Generates a new birth year. */
-  private generateBirthYear(): number {
-    return gaussian(
-      new Date().getFullYear() - 100,
-      new Date().getFullYear(),
-      0.75
-    )
+  private generateBirthYear({ year } = { year: 0 }): number {
+    year =
+      year ||
+      gaussian(new Date().getFullYear() - 100, new Date().getFullYear(), 0.75)
+
+    return year
   }
 
   /** Generates a new, full birthday. */
-  private generateBirth(): IBirth {
-    const year = this.generateBirthYear()
-    const month = this.generateBirthMonth()
-    const day = this.generateBirthDay(year, month)
+  private generateBirth(
+    { year, month, day } = { year: 0, month: 0, day: 0 }
+  ): IBirth {
+    year = year || this.generateBirthYear({ year })
+    month = month || this.generateBirthMonth({ month })
+    day = day || this.generateBirthDay(year, month)
 
     return { day, month, year }
   }
 
-  generate(): IAge {
-    return {
-      birth: this.generateBirth(),
+  generate(
+    { year, month, day } = {
+      year: 0,
+      month: 0,
+      day: 0,
     }
+  ): IAge {
+    return {
+      birth: this.generateBirth({ year, month, day }),
+    }
+  }
+
+  get monthsOld(): number {
+    const birthYear = this.birth.year
+    const birthMonth = this.birth.month
+    const currentYear = new Date().getFullYear()
+    const currentMonth = new Date().getMonth()
+    const differenceYear = currentYear - birthYear
+    const differenceMonth = currentMonth - birthMonth
+
+    return differenceYear * 12 + differenceMonth
   }
 }
 
