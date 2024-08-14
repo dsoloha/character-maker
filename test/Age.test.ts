a12ddf277bd8f2df8b5b76e17a1d28bd8ffd323c
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-14 14:00:32 -0500

    Refactored age tests

--- test/Age.test.ts
+++ test/Age.test.ts
@@ -1,31 +1,38 @@
 import { Age } from '../src/index'
+import testCharacter from './testCharacter'
+
+const defaultAge = new Age()
+const randomAge = new Age().generate()
+const givenAge = testCharacter.age
 
 test('default birthday', () => {
-  expect(new Age().birth.day).toBeGreaterThanOrEqual(1)
-  expect(new Age().birth.day).toBeLessThanOrEqual(31)
+  expect(defaultAge.birth.day).toBeGreaterThanOrEqual(1)
+  expect(defaultAge.birth.day).toBeLessThanOrEqual(31)
   expect(new Age({ birth: { month: 2 } }).birth.day).toBeLessThanOrEqual(28)
   expect(new Age({ birth: { month: 6 } }).birth.day).toBeLessThanOrEqual(30)
-  expect(new Age().birth.month).toBeGreaterThanOrEqual(1)
-  expect(new Age().birth.month).toBeLessThanOrEqual(12)
-  expect(new Age().birth.year).toBeGreaterThanOrEqual(1)
+  expect(defaultAge.birth.month).toBeGreaterThanOrEqual(1)
+  expect(defaultAge.birth.month).toBeLessThanOrEqual(12)
+  expect(defaultAge.birth.year).toBeGreaterThanOrEqual(0)
 })
 
 test('random birthday', () => {
-  expect(new Age().generate().birth?.day).toBeGreaterThanOrEqual(1)
-  expect(new Age().generate().birth?.day).toBeLessThanOrEqual(31)
-  expect(new Age().generate().birth?.month).toBeGreaterThanOrEqual(1)
-  expect(new Age().generate().birth?.month).toBeLessThanOrEqual(12)
-  expect(new Age().generate().birth?.year).toBeGreaterThanOrEqual(0)
+  expect(randomAge.birth?.day).toBeGreaterThanOrEqual(1)
+  expect(randomAge.birth?.day).toBeLessThanOrEqual(31)
+  expect(randomAge.birth?.month).toBeGreaterThanOrEqual(1)
+  expect(randomAge.birth?.month).toBeLessThanOrEqual(12)
+  expect(randomAge.birth?.year).toBeGreaterThanOrEqual(0)
 })
 
 test('given birth day', () => {
-  expect(new Age({ birth: { day: 12 } }).birth.day).toBe(12)
+  expect(givenAge.birth.day).toBeGreaterThanOrEqual(1)
+  expect(givenAge.birth.day).toBeLessThanOrEqual(31)
 })
 
 test('given birth month', () => {
-  expect(new Age({ birth: { month: 5 } }).birth.month).toBe(5)
+  expect(givenAge.birth.month).toBeGreaterThanOrEqual(1)
+  expect(givenAge.birth.month).toBeLessThanOrEqual(12)
 })
 
 test('given birth year', () => {
-  expect(new Age({ birth: { year: 99 } }).birth.year).toBe(99)
+  expect(givenAge.birth.year).toBeGreaterThanOrEqual(0)
 })
