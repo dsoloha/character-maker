497eeab4b31b54bc0ca7e95a08acf75416f8d926
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-16 08:01:48 -0500

    Refactored sex tests

--- test/Sex.test.ts
+++ test/Sex.test.ts
@@ -1,5 +1,24 @@
 import { Sex } from '../src/index'
+import testCharacter from './testCharacter'
+
+const defaultSex = new Sex()
+const randomSex = new Sex().generate()
+const givenSex = testCharacter.sex
+
+// default
 
 test('default sex', () => {
-  expect(new Sex().type).toBe('male')
+  expect(defaultSex.type).toBeDefined()
+})
+
+// random
+
+test('random sex', () => {
+  expect(randomSex.type).toBeDefined()
+})
+
+// given
+
+test('given sex', () => {
+  expect(givenSex.type).toBeDefined()
 })
