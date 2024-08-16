77b5b4e178d341338f96371e133dfd7bcd149895
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-16 09:32:50 -0500

    Refactored sexuality tests

--- test/Sexuality.test.ts
+++ test/Sexuality.test.ts
@@ -1,20 +1,45 @@
 import { Sexuality } from '../src/index'
+import testCharacter from './testCharacter'
 
-test('default sexuality', () => {
-  expect(new Sexuality('male').gender).toBe('male')
-  expect(new Sexuality('male').attraction.male).toBe(0)
-  expect(new Sexuality('male').attraction.female).toBe(0)
+const femaleSexuality = new Sexuality('female')
+const maleSexuality = new Sexuality('male')
+const givenSexuality = testCharacter.sexuality
+
+// female
+
+test('female gender', () => {
+  expect(femaleSexuality.gender).toBeDefined()
 })
 
+test('female attraction', () => {
+  expect(femaleSexuality.attraction.female).toBeGreaterThanOrEqual(0)
+  expect(femaleSexuality.attraction.female).toBeLessThanOrEqual(100)
+  expect(femaleSexuality.attraction.male).toBeGreaterThanOrEqual(0)
+  expect(femaleSexuality.attraction.male).toBeLessThanOrEqual(100)
+})
+
+// male
+
+test('male gender', () => {
+  expect(maleSexuality.gender).toBeDefined()
+})
+
+test('male attraction', () => {
+  expect(maleSexuality.attraction.female).toBeGreaterThanOrEqual(0)
+  expect(maleSexuality.attraction.female).toBeLessThanOrEqual(100)
+  expect(maleSexuality.attraction.male).toBeGreaterThanOrEqual(0)
+  expect(maleSexuality.attraction.male).toBeLessThanOrEqual(100)
+})
+
+// given
+
 test('given gender', () => {
-  expect(new Sexuality('male', { gender: 'female' }).gender).toBe('female')
+  expect(givenSexuality.gender).toBeDefined()
 })
 
 test('given attraction', () => {
-  expect(
-    new Sexuality('male', { attraction: { male: 0, female: 100 } }).attraction
-  ).toEqual({ male: 0, female: 100 })
-  expect(
-    new Sexuality('male', { attraction: { male: 100, female: 0 } }).attraction
-  ).toEqual({ male: 100, female: 0 })
+  expect(givenSexuality.attraction.female).toBeGreaterThanOrEqual(0)
+  expect(givenSexuality.attraction.female).toBeLessThanOrEqual(100)
+  expect(givenSexuality.attraction.male).toBeGreaterThanOrEqual(0)
+  expect(givenSexuality.attraction.male).toBeLessThanOrEqual(100)
 })
