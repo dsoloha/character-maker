dcf4c095bf280509f75caf4387d5acb5bc125432
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-15 19:11:17 -0500

    Formatting

--- test/Arms.test.ts
+++ test/Arms.test.ts
@@ -5,15 +5,21 @@ const defaultArms = new Arms()
 const randomArms = new Arms().generate(20)
 const givenArms = testCharacter.arms
 
+// default
+
 test('default arm size', () => {
   expect(defaultArms.left.size).toBeDefined()
   expect(defaultArms.right.size).toBeDefined()
 })
 
+// random
+
 test('random arm size', () => {
   expect(randomArms.left?.size).toBeDefined()
   expect(randomArms.right?.size).toBeDefined()
 })
+
+// given
 
 test('given arm size', () => {
   expect(givenArms?.left.size).not.toBeNaN()
