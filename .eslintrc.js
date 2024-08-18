3d3d9826891e0312e8e4fea4e0e5e737bae38ced
DSoloha <38593903+dsoloha@users.noreply.github.com>
2024-08-18 10:29:38 -0500

    Added eslint for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:jest-extended/all',
+  ],
   parserOptions: {
     sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'jest-extended'],
 }
