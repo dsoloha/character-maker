14e69183650f5986577cd8b53080b25d9aaa1caf
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-04 22:51:21 -0500

    Updated IFace to use optional properties

--- src/Face.ts
+++ src/Face.ts
@@ -9,6 +9,8 @@ export default class Face implements IFace {
 }
 
 interface IFace {
-  attractiveness: number
-  description: string
+  /** The character's attractiveness, on a scale from 1-100. */
+  attractiveness?: number
+  /** A description of the character's face. */
+  description?: string
 }
