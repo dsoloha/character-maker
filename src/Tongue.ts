9e8562b00be92a22a63a9d1b02a376fa0cc7875e
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-04 22:49:15 -0500

    Added tongue docs, Tongue class

---
+++ src/Tongue.ts
@@ -0,0 +1,17 @@
+export default class Tongue implements ITongue {
+  ageusia: boolean
+  description: string
+
+  constructor() {
+    this.ageusia = false
+    this.description = 'normal'
+  }
+}
+
+export interface ITongue {
+  /** Whether or not the character lacks a sense of taste. */
+  ageusia: boolean
+
+  /** A description of the character's tongue. */
+  description: string
+}
