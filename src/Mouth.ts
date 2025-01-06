3c2512214a0b6d272db6a93a3f7b504dac7324b2
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-06 16:59:47 -0500

    Updated Mouth class to use new classes

--- src/Mouth.ts
+++ src/Mouth.ts
@@ -1,54 +1,27 @@
 import '../lib/array'
-import { gaussian, random } from '../lib/number'
+import Teeth, { ITeeth } from './Teeth'
+import Tongue, { ITongue } from './Tongue'
 
 export default class Mouth implements IMouth {
-  teeth: {
-    count: number
-    description: string
-  }
-  tongue: boolean
+  teeth: ITeeth
+  tongue: ITongue
 
   constructor(options?: IMouth) {
-    this.teeth = options?.teeth ?? {
-      count: 32,
-      description: '',
-    }
-    this.tongue = options?.tongue ?? true
-  }
-
-  generateTeeth(): ITeeth {
-    const count = gaussian(1, 100) > 95 ? gaussian(1, 32, 0.25) : 32
-    const descriptions = ['straight', 'yellow', 'white']
-    const description = descriptions.random()
-
-    return {
-      count,
-      description,
-    }
-  }
-
-  generateTongue(): boolean {
-    return gaussian(1, 100) <= 99
+    this.teeth = options?.teeth ?? new Teeth()
+    this.tongue = options?.tongue ?? new Tongue()
   }
 
   generate(): IMouth {
     return {
-      teeth: this.generateTeeth(),
-      tongue: this.generateTongue(),
+      teeth: new Teeth().generate(),
+      tongue: new Tongue().generate(),
     }
   }
-}
-
-export interface ITeeth {
-  /** How many teeth the character has, out of 32. */
-  count: number
-  /** A description of the character's teeth. */
-  description: string
 }
 
 export interface IMouth {
   /** Properties pertaining to the character's teeth. */
   teeth?: ITeeth
-  /** Whether or not the character has a tongue. */
-  tongue?: boolean
+  /** Properties pertaining to the character's tongue. */
+  tongue?: ITongue
 }
