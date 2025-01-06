b7c4f523b59302064529d42a5a4eeb795462e2ba
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-06 17:06:44 -0500

    Added .description property to Neck class

--- src/Neck.ts
+++ src/Neck.ts
@@ -1,24 +1,38 @@
 import { gaussian } from '../lib/number'
 
 export default class Neck implements INeck {
+  description: string
   larynx: boolean
 
   constructor(options?: INeck) {
-    this.larynx = options?.larynx ?? true
+    this.description = options?.description ?? this.generateDescription()
+    this.larynx = options?.larynx ?? this.generateLarynx()
   }
 
-  generateLarynx(): boolean {
-    return gaussian(1, 100) > 99 ? false : true
+  /** Generates a description of the neck. */
+  private generateDescription(): string {
+    const descriptions = ['average', 'normal']
+    const description = descriptions.random()
+
+    return description
+  }
+
+  /** Generates whether the character has vocal cords. */
+  private generateLarynx(): boolean {
+    return gaussian(1, 100) <= 99
   }
 
   generate(): INeck {
     return {
+      description: this.generateDescription(),
       larynx: this.generateLarynx(),
     }
   }
 }
 
 export interface INeck {
-  /** Whether or not the character has vocal cords. */
+  /** A description of the character's neck. */
+  description?: string
+  /** Whether the character has vocal cords. */
   larynx?: boolean
 }
