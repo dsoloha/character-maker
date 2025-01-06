fb81cae86d658c423d058f42a6ca3977c6c7d3fb
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-06 16:59:16 -0500

    Added generator functions to Teeth, Tongue classes

--- src/Teeth.ts
+++ src/Teeth.ts
@@ -1,3 +1,5 @@
+import { gaussian } from '../lib/number'
+
 export default class Teeth implements ITeeth {
   count: number
   description: string
@@ -5,6 +7,26 @@ export default class Teeth implements ITeeth {
   constructor() {
     this.count = 32
     this.description = 'normal'
+  }
+
+  /** Generates the number of teeth. */
+  private generateCount(): number {
+    return gaussian(1, 100) > 95 ? gaussian(1, 32, 0.25) : 32
+  }
+
+  /** Generates a description of the teeth. */
+  private generateDescription(): string {
+    const descriptions = ['straight', 'yellow', 'white']
+    const description = descriptions.random()
+
+    return description
+  }
+
+  generate(): ITeeth {
+    return {
+      count: this.generateCount(),
+      description: this.generateDescription(),
+    }
   }
 }
 
--- src/Tongue.ts
+++ src/Tongue.ts
@@ -1,3 +1,5 @@
+import { gaussian } from '../lib/number'
+
 export default class Tongue implements ITongue {
   ageusia: boolean
   description: string
@@ -5,6 +7,26 @@ export default class Tongue implements ITongue {
   constructor() {
     this.ageusia = false
     this.description = 'normal'
+  }
+
+  /** Generates whether the tongue lacks a sense of taste. */
+  private generateAgeusia(): boolean {
+    return gaussian(1, 100) <= 99
+  }
+
+  /** Generates a description of the tongue. */
+  private generateDescription(): string {
+    const descriptions = ['average', 'normal']
+    const description = descriptions.random()
+
+    return description
+  }
+
+  generate(): ITongue {
+    return {
+      ageusia: this.generateAgeusia(),
+      description: this.generateDescription(),
+    }
   }
 }
 
