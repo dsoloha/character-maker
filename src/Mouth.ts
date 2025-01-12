8bc2bb073467e3ca2f8c8df4fa914b60ee7e3761
DSoloha <38593903+dsoloha@users.noreply.github.com>
2025-01-11 22:50:09 -0500

    Added .lips property to Mouth class

--- src/Mouth.ts
+++ src/Mouth.ts
@@ -1,18 +1,22 @@
 import '../lib/array'
+import Lips, { ILips } from './Lips'
 import Teeth, { ITeeth } from './Teeth'
 import Tongue, { ITongue } from './Tongue'
 
 export default class Mouth implements IMouth {
+  lips: ILips
   teeth: ITeeth
   tongue: ITongue
 
   constructor(options?: IMouth) {
+    this.lips = options?.lips ?? new Lips()
     this.teeth = options?.teeth ?? new Teeth()
     this.tongue = options?.tongue ?? new Tongue()
   }
 
   generate(): IMouth {
     return {
+      lips: new Lips(),
       teeth: new Teeth().generate(),
       tongue: new Tongue().generate(),
     }
@@ -20,6 +24,8 @@ export default class Mouth implements IMouth {
 }
 
 export interface IMouth {
+  /** Properties pertaining to the character's lips. */
+  lips?: ILips
   /** Properties pertaining to the character's teeth. */
   teeth?: ITeeth
   /** Properties pertaining to the character's tongue. */
